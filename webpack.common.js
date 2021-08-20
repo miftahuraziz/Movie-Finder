f9ac1973f07a9f9eaa4d59dfa40e8921724933fd
miftahuraziz <miftahuraziz@gmail.com>
2021-08-20 15:03:32 +0700

    change production folder name output

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "docs"),
         filename: "[name].bundle.js"
     },
     optimization: {
